101424fb3f7858f161196bc51bedbd6122f8e51d
ilyana19 <aries_2152@hotmail.com>
2018-07-22 23:14:08 -0400

    wip, moved around some functions and added basic level up check

--- ex3-player-ver2.rb
+++ ex3-player-ver2.rb
@@ -44,6 +44,14 @@ class Player
     return @ac
   end
 
+  def event
+    return @event
+  end
+
+  def gold
+    return @gold
+  end
+
   def initiative
     return @initiative
   end
@@ -59,17 +67,10 @@ class Player
   def current_exp=(n)
     return @exp = n
   end
-  ######################
 
-  # get action/event info
-  def event
-    return @event
+  def current_level=(n)
+    return @current_level = n
   end
-
-  def gold
-    return @gold
-  end
-  ######################
 
   # actions
   def adventure
@@ -87,6 +88,28 @@ class Player
     @gold = Random.rand(1..20)
     @wallet += @gold
   end
+
+  def level_up
+    if @exp >= 100
+      @lvl = 2
+    elsif @exp >= 300
+      @lvl = 3
+    end
+  end
+
+  def get_stats
+    puts <<~TEXT
+
+      =======================
+      #{name} Stats
+      =======================
+      Level: #{current_level}
+      HP: #{current_hp}
+      Gold: #{wallet}
+      EXP: #{current_exp}
+
+    TEXT
+  end
 end
 
 # declare variables
@@ -95,6 +118,14 @@ player = Player.new("Faye Phlorik")
 # determine initiative (who attacks first)
 def roll_initiative
   return initiative = Random.rand(1..20)
+end
+
+def create_monster
+  # randomize monster stats (should really be another object...)
+  # monster_hp = 2 * Random.rand(1..6)
+  monster_hp = 5
+  monster_ac = 15
+  return monster_hp, monster_ac
 end
 
 puts <<~TEXT
@@ -109,58 +140,71 @@ puts <<~TEXT
   ==========================================
 TEXT
 
-5.times do
-  if player.current_hp >= 1
-    player.adventure
-    if player.event == "monster"
-      puts "#{player.name} encountered a monster."
-      puts ""
-    
-      # randomize monster stats (should really be another object...)
-      # monster_hp = 2 * Random.rand(1..6)
-      monster_hp = 5
-      monster_ac = 15
-
-      # roll initiative
-      player.initiative = roll_initiative
-      monster_initiative = roll_initiative
-
-      while monster_hp > 0
-        monster_atk = Random.rand(1..20)
-        monster_dmg = Random.rand(1..6)
-        player_atk = Random.rand(1..20)
-        player_dmg = Random.rand(1..8)
-
-        # check if monster hits
-        if monster_atk >= player.ac
-          puts "Monster dealt #{monster_dmg} damage."
-          player.current_hp -= monster_dmg
-        else
-          puts "Monster misses #{player.name}."
-        end
-
-        # check if player hits
-        if player_atk >= monster_ac
-          puts "#{player.name} dealt #{player_dmg} damage."
-          monster_hp -= player_dmg
-        else
-          puts "#{player.name} misses."
-        end
-      end
-
-      #show winner
-      if monster_hp == 0
-        gained_exp = Random.rand(50..75)
-        player.current_exp += gained_exp
-        puts "\n*** Congratulations! ***\n#{player.name} gained #{gained_exp} exp."
-      end
-    elsif player.event == "treasure"
-      puts "#{player.name} found a treasure."
-      player.collect_treasure
-      puts "#{player.name} opened a chest with #{player.gold} gold pieces in it."
-    end
+loop do
+  player.adventure
+  if player.event == "monster"
+    puts "#{player.name} encountered a monster."
     puts ""
-  else
+
+    monster_hp = create_monster[0]
+    monster_ac = create_monster[1]
+
+    while monster_hp > 0 && player.current_hp > 0
+      monster_atk = Random.rand(1..20)
+      monster_dmg = Random.rand(1..6)
+      player_atk = Random.rand(1..20)
+      player_dmg = Random.rand(1..8)
+
+      # check if monster hits
+      if monster_atk >= player.ac
+        puts "Monster dealt #{monster_dmg} damage."
+        player.current_hp -= monster_dmg
+        if player.current_hp == 0
+          break
+        end
+      else
+        puts "Monster misses #{player.name}."
+      end
+      # check if player hits
+      if player_atk >= monster_ac
+        puts "#{player.name} dealt #{player_dmg} damage."
+        monster_hp -= player_dmg
+        if monster_hp == 0
+          break
+        end
+      else
+        puts "#{player.name} misses."
+      end
+    end
+
+    # show how much exp earned while player is still alive
+    if player.current_hp > 0
+      gained_exp = Random.rand(50..75)
+      player.current_exp += gained_exp
+      puts <<~TEXT
+
+        ********************************************
+        * Congratulations!
+        *
+        * #{player.name} gained #{gained_exp} exp.
+        ********************************************
+      TEXT
+
+      # check level up
+      player.level_up
+      player.get_stats
+    end
+
+  elsif player.event == "treasure"
+    puts "#{player.name} found a treasure."
+    player.collect_treasure
+    puts "#{player.name} opened a chest with #{player.gold} gold pieces in it."
+  end
+
+  puts ""
+
+  # end the adventure loop
+  if player.current_hp < 1
     puts "#{player.name} died."
     break
   end
